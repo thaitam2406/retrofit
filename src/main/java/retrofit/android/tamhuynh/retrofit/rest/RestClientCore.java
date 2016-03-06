36e4b1401f6c2fd2095f11038a146426b2fab945
Tam Huynh <tam.huynh@audiencemedia.com>
2016-03-06 16:01:51 +0700

    update rxjava

--- src/main/java/retrofit/android/tamhuynh/retrofit/rest/RestClientCore.java
+++ src/main/java/retrofit/android/tamhuynh/retrofit/rest/RestClientCore.java
@@ -3,11 +3,13 @@ package retrofit.android.tamhuynh.retrofit.rest;
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Response;
+import com.squareup.okhttp.logging.HttpLoggingInterceptor;
 
 import java.io.IOException;
 
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
 
 /**
  * Created by tamhuynh on 1/31/16.
@@ -19,7 +21,14 @@ public class RestClientCore {
     public synchronized static APIServerCore getClient() {
         if (apiServerCoreInterface == null) {
 
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            // set your desired log level
+            logging.setLevel(HttpLoggingInterceptor.Level.HEADERS);
+            logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
             OkHttpClient okClient = new OkHttpClient();
+            okClient.interceptors().add(logging);
             okClient.interceptors().add(new Interceptor() {
                 @Override
                 public Response intercept(Chain chain) throws IOException {
@@ -31,11 +40,12 @@ public class RestClientCore {
             Retrofit client = new Retrofit.Builder()
                     .baseUrl(APIConfigCore.domainAPI)
                     .client(okClient)
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
             apiServerCoreInterface = client.create(APIServerCore.class);
         }
-        return apiServerCoreInterface;
+        return apiServerCoreInterface ;
     }
 
 }
